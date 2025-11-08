277596203b8054dc7e0ad2eba793c9d6e15f40ad
Alexandra Khalimon <lexikhalimon@gmail.com>
2025-11-08 23:11:50 +0200

    add auth-routes

---
+++ src/models/user.js
@@ -0,0 +1,33 @@
+import { model, Schema } from "mongoose";
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      required: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      trim: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false
+  }
+);
+
+userSchema.methods.toJSON = function () {
+  const obj = this.toObject();
+  delete obj.password;
+  return obj;
+};
+
+export const User = model('User', userSchema);
--- src/validations/authValidation.js
+++ src/validations/authValidation.js
@@ -1,7 +1,29 @@
-export const resetPasswordSchema = {}
+import { Joi, Segments } from "celebrate";
 
-export const requestResetEmailSchema = {}
+export const resetPasswordSchema = {
+  [Segments.BODY]: Joi.object({
+    password: Joi.string().min(8).max(128).required(),
+    token: Joi.string().required(),
+  }),
+};
 
-export const registerUserSchema = {}
+export const requestResetEmailSchema = {
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().email().max(64).required(),
+  }),
+};
 
-export const loginUserSchema = {}
+export const registerUserSchema = {
+  [Segments.BODY]: Joi.object({
+    name: Joi.string().max(32).required(),
+    email: Joi.string().email().max(64).required(),
+    password: Joi.string().min(8).max(128).required(),
+  }),
+};
+
+export const loginUserSchema = {
+  [Segments.BODY]: Joi.object({
+    email: Joi.string().email().max(64).required(),
+    password: Joi.string().min(8).max(128).required(),
+  }),
+}
